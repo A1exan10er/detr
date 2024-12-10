4453dc50b38a09b1bb9130dad10b34ed36953dda
Tianyu Yang <tianyu.yang@ipa.fraunhofer.de>
2024-12-10 15:42:33 +0100

    Add WandB

--- main.py
+++ main.py
@@ -16,8 +16,79 @@ from datasets import build_dataset, get_coco_api_from_dataset
 from engine import evaluate, train_one_epoch
 from models import build_model
 
+# WandB configuration
+import wandb
+import random
+
+# WandB initialization and logging
+
+'''
+# simulate training
+epochs = 10
+offset = random.random() / 5
+for epoch in range(2, epochs):
+    acc = 1 - 2 ** -epoch - random.random() / epoch - offset
+    loss = 2 ** -epoch + random.random() / epoch + offset
+
+    # log metrics to wandb
+    wandb.log({"acc": acc, "loss": loss})
+'''
+'''
+# [optional] finish the wandb run, necessary in notebooks
+wandb.finish()
+# End of WandB configuration
+'''
+
 
 def get_args_parser():
+    """
+    Creates an argument parser for the transformer detector.
+
+    Returns:
+        argparse.ArgumentParser: The argument parser with the following arguments:
+            --lr (float): Learning rate. Default is 1e-4.
+            --lr_backbone (float): Learning rate for the backbone. Default is 1e-5.
+            --batch_size (int): Batch size. Default is 2.
+            --weight_decay (float): Weight decay. Default is 1e-4.
+            --epochs (int): Number of epochs. Default is 300.
+            --lr_drop (int): Epoch to drop learning rate. Default is 200.
+            --clip_max_norm (float): Gradient clipping max norm. Default is 0.1.
+            --frozen_weights (str): Path to the pretrained model. If set, only the mask head will be trained. Default is None.
+            --backbone (str): Name of the convolutional backbone to use. Default is 'resnet50'.
+            --dilation (bool): If true, replaces stride with dilation in the last convolutional block (DC5).
+            --position_embedding (str): Type of positional embedding to use on top of the image features. Choices are 'sine' and 'learned'. Default is 'sine'.
+            --enc_layers (int): Number of encoding layers in the transformer. Default is 6.
+            --dec_layers (int): Number of decoding layers in the transformer. Default is 6.
+            --dim_feedforward (int): Intermediate size of the feedforward layers in the transformer blocks. Default is 2048.
+            --hidden_dim (int): Size of the embeddings (dimension of the transformer). Default is 256.
+            --dropout (float): Dropout applied in the transformer. Default is 0.1.
+            --nheads (int): Number of attention heads inside the transformer's attentions. Default is 8.
+            --num_queries (int): Number of query slots. Default is 100.
+            --pre_norm (bool): If true, applies pre-normalization in the transformer.
+            --masks (bool): If true, trains the segmentation head.
+            --no_aux_loss (bool): If true, disables auxiliary decoding losses (loss at each layer).
+            --set_cost_class (float): Class coefficient in the matching cost. Default is 1.
+            --set_cost_bbox (float): L1 box coefficient in the matching cost. Default is 5.
+            --set_cost_giou (float): GIoU box coefficient in the matching cost. Default is 2.
+            --mask_loss_coef (float): Coefficient for the mask loss. Default is 1.
+            --dice_loss_coef (float): Coefficient for the dice loss. Default is 1.
+            --bbox_loss_coef (float): Coefficient for the bounding box loss. Default is 5.
+            --giou_loss_coef (float): Coefficient for the GIoU loss. Default is 2.
+            --eos_coef (float): Relative classification weight of the no-object class. Default is 0.1.
+            --dataset_file (str): Name of the dataset file. Default is 'coco'.
+            --coco_path (str): Path to the COCO dataset.
+            --coco_panoptic_path (str): Path to the COCO panoptic dataset.
+            --remove_difficult (bool): If true, removes difficult examples from the dataset.
+            --output_dir (str): Path where to save the output. Default is '' (no saving).
+            --device (str): Device to use for training/testing. Default is 'cuda'.
+            --seed (int): Random seed. Default is 42.
+            --resume (str): Path to resume from checkpoint. Default is ''.
+            --start_epoch (int): Epoch to start training from. Default is 0.
+            --eval (bool): If true, evaluates the model.
+            --num_workers (int): Number of data loading workers. Default is 2.
+            --world_size (int): Number of distributed processes. Default is 1.
+            --dist_url (str): URL used to set up distributed training. Default is 'env://'.
+    """
     parser = argparse.ArgumentParser('Set transformer detector', add_help=False)
     parser.add_argument('--lr', default=1e-4, type=float)
     parser.add_argument('--lr_backbone', default=1e-5, type=float)
@@ -102,7 +173,104 @@ def get_args_parser():
     return parser
 
 
+'''
+# start a new wandb run to track this script
+wandb.init(
+    # set the wandb project where this run will be logged
+    project="DETR",
+
+    # track hyperparameters and run metadata
+    config={
+    "learning_rate": 0.02,
+    "architecture": "CNN",
+    "dataset": "COCO2017",
+    "epochs": 10,
+    }
+)
+'''
+# WandB configuration
+# Initialize the parser
+parser = get_args_parser()
+args = parser.parse_args()
+# Log the parameters to WandB
+# # Start a new WandB run and log all args parameters
+# wandb.init(
+#     project="DETR",
+#     config=vars(args)  # Convert args Namespace to dictionary
+# )
+# Start a new WandB run and log the parameters with specific names
+wandb.init(
+    project="DETR",
+    config={
+        "learning_rate": args.lr,
+        "learning_rate_backbone": args.lr_backbone,
+        "batch_size": args.batch_size,
+        "weight_decay": args.weight_decay,
+        "epochs": args.epochs,
+        "lr_drop": args.lr_drop,
+        "clip_max_norm": args.clip_max_norm,
+        "frozen_weights": args.frozen_weights,
+        "backbone": args.backbone,
+        "dilation": args.dilation,
+        "position_embedding": args.position_embedding,
+        "enc_layers": args.enc_layers,
+        "dec_layers": args.dec_layers,
+        "dim_feedforward": args.dim_feedforward,
+        "hidden_dim": args.hidden_dim,
+        "dropout": args.dropout,
+        "nheads": args.nheads,
+        "num_queries": args.num_queries,
+        "pre_norm": args.pre_norm, #check here
+        "masks": args.masks,
+        "aux_loss": args.aux_loss, #check here
+        "set_cost_class": args.set_cost_class,
+        "set_cost_bbox": args.set_cost_bbox,
+        "set_cost_giou": args.set_cost_giou,
+        "mask_loss_coef": args.mask_loss_coef,
+        "dice_loss_coef": args.dice_loss_coef,
+        "bbox_loss_coef": args.bbox_loss_coef,
+        "giou_loss_coef": args.giou_loss_coef,
+        "eos_coef": args.eos_coef,
+        "dataset_file": args.dataset_file,
+        "coco_path": args.coco_path,
+        "coco_panoptic_path": args.coco_panoptic_path,
+        "remove_difficult": args.remove_difficult,
+        "output_dir": args.output_dir,
+        "device": args.device,
+        "seed": args.seed,
+        "resume": args.resume,
+        "start_epoch": args.start_epoch,
+        "eval": args.eval,
+        "num_workers": args.num_workers,
+        "world_size": args.world_size,
+        "dist_url": args.dist_url
+    }
+)
+
+
 def main(args):
+    """
+    Main function to train and evaluate the model.
+
+    Args:
+        args (Namespace): Arguments for training and evaluation.
+
+    The function performs the following steps:
+    1. Initializes distributed mode if specified.
+    2. Sets the random seed for reproducibility.
+    3. Builds the model, criterion, and postprocessors.
+    4. Moves the model to the specified device.
+    5. Wraps the model with DistributedDataParallel if in distributed mode.
+    6. Sets up the optimizer and learning rate scheduler.
+    7. Builds the training and validation datasets and data loaders.
+    8. Loads frozen weights if specified.
+    9. Resumes from a checkpoint if specified.
+    10. Evaluates the model if in evaluation mode.
+    11. Trains the model for the specified number of epochs.
+    12. Saves checkpoints and evaluation results periodically.
+    13. Logs training and evaluation statistics.
+    14. Prints the total training time.
+    """
     utils.init_distributed_mode(args)
     print("git:\n  {}\n".format(utils.get_sha()))
 
@@ -234,6 +402,16 @@ def main(args):
                     for name in filenames:
                         torch.save(coco_evaluator.coco_eval["bbox"].eval,
                                    output_dir / "eval" / name)
+        '''
+        # Log metrics to WandB
+        wandb.log({  # <--- WandB Logging
+            'epoch': epoch,
+            'train_loss': train_stats['loss'],
+            'train_accuracy': train_stats.get('accuracy', 0),
+            'val_loss': test_stats['loss'],
+            'val_accuracy': test_stats.get('accuracy', 0),
+            'learning_rate': optimizer.param_groups[0]["lr"]
+        })'''
 
     total_time = time.time() - start_time
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
