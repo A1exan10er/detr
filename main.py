923bb4e093470094f4113f955df1c7f49cbe0548
Tianyu Yang <tianyu.yang@ipa.fraunhofer.de>
2024-12-10 15:53:29 +0100

    Remove redundant

--- main.py
+++ main.py
@@ -20,25 +20,6 @@ from models import build_model
 import wandb
 import random
 
-# WandB initialization and logging
-
-'''
-# simulate training
-epochs = 10
-offset = random.random() / 5
-for epoch in range(2, epochs):
-    acc = 1 - 2 ** -epoch - random.random() / epoch - offset
-    loss = 2 ** -epoch + random.random() / epoch + offset
-
-    # log metrics to wandb
-    wandb.log({"acc": acc, "loss": loss})
-'''
-'''
-# [optional] finish the wandb run, necessary in notebooks
-wandb.finish()
-# End of WandB configuration
-'''
-
 
 def get_args_parser():
     """
@@ -173,21 +154,6 @@ def get_args_parser():
     return parser
 
 
-'''
-# start a new wandb run to track this script
-wandb.init(
-    # set the wandb project where this run will be logged
-    project="DETR",
-
-    # track hyperparameters and run metadata
-    config={
-    "learning_rate": 0.02,
-    "architecture": "CNN",
-    "dataset": "COCO2017",
-    "epochs": 10,
-    }
-)
-'''
 # WandB configuration
 # Initialize the parser
 parser = get_args_parser()
